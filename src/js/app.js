e268291cd1941f3992ad2bcc086e8939c1dd97bf
Sandeep Sala <sandeepsala@outlook.com>
2020-05-30 15:57:22 +0530

    bug fixxed

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 function send(el){
-    document.getElementById("loader").style.display = "block"
-    var txt = el.value
-    var data = count(txt);
+    var loader = document.getElementById("loader");
+    loader.style.display = "block";
+    var txt    = el.value.replace( /[\r\n]+/gm, " " );
+    var data   = count(txt);
     document.getElementById("word-count").innerHTML = data.word_count;
     document.getElementById("letter-count").innerHTML = data.letter_count;
     dat = data.sorted_word
@@ -14,21 +15,21 @@ function send(el){
         html+= "<tr><td> - </td><td> - </td><td> - </td></tr>"
     }
     document.getElementById("counter-table").innerHTML = html
-    setTimeout(function(){ 
-        document.getElementById("loader").style.display = "none"
-        }, 500);
+    setTimeout(function(){ loader.style.display = "none";}, 500);
 }
+
 
 function count(el){
     var word_dict = {}
     var word_list  = el.trim().split(" ")
-    var word_count =  word_list.length
+    var word_count =  0
     if ( word_list.length == 1 && word_list[0] == "" ){word_count = 0}
-    letter_count = (el.trim().replace(" ","")).length 
-    for(i=0;i<word_count;i++){
-        if(word_list[i] != ''){
+    letter_count = (el.trim().replace(/ /g,"")).length 
+    for(i=0;i<word_list.length ;i++){
+        if(word_list[i].trim().replace(' ','') != ''){
             if(word_list[i] in word_dict){ word_dict[word_list[i]] += 1 }
             else{ word_dict[word_list[i]] = 1 }
+            word_count += 1;
         }
     }
     var Data = { 
