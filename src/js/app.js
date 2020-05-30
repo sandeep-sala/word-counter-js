1114b4fe90e08c43cd97fc3617790ad49a220a3e
Sandeep Sala <sandeepsala@outlook.com>
2020-05-30 13:09:01 +0530

    function updated

--- src/js/app.js
+++ src/js/app.js
@@ -3,28 +3,39 @@ function send(el){
     var ajax = new XMLHttpRequest();
     var csrf = document.getElementsByTagName('input')[0].value
     var txt = el.value
-    ajax.open("POST", "count/", true);
-    ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    ajax.send("csrfmiddlewaretoken="+csrf+"&txt="+txt);
-    ajax.onreadystatechange = function() {
-        if (ajax.readyState == 4 && ajax.status == 200) {
-            var data = JSON.parse(ajax.responseText);
-            document.getElementById("word-count").innerHTML = data.word_count;
-            document.getElementById("letter-count").innerHTML = data.letter_count;
-            dat = data.sorted_word
-            html = ''
-            if (dat.length !== 0){
-                for (var i=0 ;i<(dat).length; i++){
-                    html+= "<tr><td>"+String(i+1)+"</td><td>"+dat[i][0]+"</td><td>"+String(dat[i][1])+"</td></tr>"
-                }
-            }else{
-                html+= "<tr><td> - </td><td> - </td><td> - </td></tr>"
-            }
-            document.getElementById("counter-table").innerHTML = html
-            setTimeout(function(){ 
-                document.getElementById("loader").style.display = "none"
-             }, 500);  
-            
+    var data = count(txt);
+    document.getElementById("word-count").innerHTML = data.word_count;
+    document.getElementById("letter-count").innerHTML = data.letter_count;
+    dat = data.sorted_word
+    html = ''
+    if (dat.length !== 0){
+        for (var i=0 ;i<(dat).length; i++){
+            html+= "<tr><td>"+String(i+1)+"</td><td>"+dat[i][0]+"</td><td>"+String(dat[i][1])+"</td></tr>"
+        }
+    }else{
+        html+= "<tr><td> - </td><td> - </td><td> - </td></tr>"
+    }
+    document.getElementById("counter-table").innerHTML = html
+    setTimeout(function(){ 
+        document.getElementById("loader").style.display = "none"
+        }, 500);
+}
+
+function count(el){
+    var word_dict = {}
+    var word_list  = el.trim().split(" ")
+    if ( word_list.length == 1 && word_list[0] == "" ){word_count = 0}
+    letter_count = word_list.length 
+    for(i=0;i<letter_count;i++){
+        if(word_list[i] != ''){
+            if(word_list[i] in word_dict){ word_dict[word_list[i]] += 1 }
+            else{ word_dict[word_list[i]] = 1 }
         }
     }
+    var Data = { 
+            'sorted_word' : Object.entries(word_dict).sort((a, b) => b[1] - a[1]),
+            'word_count'  : word_count,
+            'letter_count': letter_count
+        }
+    return Data    
 }
